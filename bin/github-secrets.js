8c3e95fe38a715ffb8bba483d7e8ba76de9ac6ff
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2021-01-13 19:27:06 +0000

    Add support for organisation secrets
    Also:
    * made repository optional
    * extracted to methods to reduce duplication and improve legibility
    * bump version number

--- bin/github-secrets.js
+++ bin/github-secrets.js
@@ -19,7 +19,6 @@ const builder = {
     describe: 'Github repository owner'
   },
   r: {
-    demandOption: true,
     alias: 'repository',
     nargs: 1,
     describe: 'Github repository'
@@ -53,37 +52,46 @@ function encrypt (publicKey, value) {
   return encrypted
 }
 
-async function putSecrets (accessToken, filename, owner, repo) {
+function getSecretsPath (owner, repository) {
+  const [path, parameters] = repository
+    ? ['/repos/{owner}/{repository}/actions/secrets', { owner, repository }]
+    : ['/orgs/{owner}/actions/secrets', { owner, visibility: 'all' }]
+  return { path, parameters }
+}
+
+async function githubRequest (accessToken, restCmd, options) {
   const octokit = new Octokit({ auth: accessToken })
-  const response = await octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
-    owner,
-    repo
-  })
-  const fileStream = fs.createReadStream(filename)
-  const publicKey = response.data.key
-  const publicKeyId = response.data.key_id
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  })
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
-
-  for await (const line of rl) {
-    const [key, value] = line.split('=')
-    await octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-      owner,
-      repo,
-      secret_name: key,
-      encrypted_value: encrypt(publicKey, value),
-      key_id: publicKeyId
-    })
+  try {
+    // console.log({ restCmd, options })
+    return await octokit.request(restCmd, options)
+  } catch (err) {
+    throw Error(`githubRequest failed (status ${err.status}): ${restCmd} ${JSON.stringify(options)}`)
   }
 }
 
-async function deleteSecrets (accessToken, filename, owner, repo) {
-  const octokit = new Octokit({ auth: accessToken })
+async function getPublicKey (accessToken, owner, repository) {
+  const { path, parameters } = getSecretsPath(owner, repository)
+  const restCmd = `GET ${path}/public-key`
+  const response = await githubRequest(accessToken, restCmd, parameters)
+  return { publicKey: response.data.key, publicKeyId: response.data.key_id }
+}
+
+async function checkSecretsSupported (accessToken, owner, repository) {
+  if (!repository) {
+    const { data: { type } } = await githubRequest(accessToken, '/users/{owner}', { owner })
+    if (type === 'User') {
+      console.log(`${owner} is a user and a repository has not been specified.\nSecrets can only be stored for repositorys and organisations. `)
+      return false
+    }
+  }
+  return true
+}
+
+async function putSecrets (accessToken, filename, owner, repository) {
+  if (!await checkSecretsSupported(accessToken, owner, repository)) {
+    return
+  }
+  const { publicKey, publicKeyId } = await getPublicKey(accessToken, owner, repository)
   const fileStream = fs.createReadStream(filename)
 
   const rl = readline.createInterface({
@@ -93,12 +101,41 @@ async function deleteSecrets (accessToken, filename, owner, repo) {
   // Note: we use the crlfDelay option to recognize all instances of CR LF
   // ('\r\n') in input.txt as a single line break.
 
+  const { path, parameters: pathParameters } = getSecretsPath(owner, repository)
+  for await (const line of rl) {
+    const [key, value] = line.split('=')
+    const restCmd = `PUT ${path}/{secret_name}`
+    const parameters = {
+      secret_name: key,
+      encrypted_value: encrypt(publicKey, value),
+      key_id: publicKeyId,
+      ...pathParameters
+    }
+    await githubRequest(accessToken, restCmd, parameters)
+  }
+}
+
+async function deleteSecrets (accessToken, filename, owner, repository) {
+  if (!await checkSecretsSupported(accessToken, owner, repository)) {
+    return
+  }
+
+  const fileStream = fs.createReadStream(filename)
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  const { path, parameters: pathParameters } = getSecretsPath(owner, repository)
+  const restCmd = `DELETE ${path}/{secret_name}`
   for await (const line of rl) {
     const [key] = line.split('=')
-    await octokit.request('DELETE /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-      owner,
-      repo,
-      secret_name: key
-    })
+    const options = {
+      secret_name: key,
+      ...pathParameters
+    }
+    await githubRequest(accessToken, restCmd, options)
   }
 }
